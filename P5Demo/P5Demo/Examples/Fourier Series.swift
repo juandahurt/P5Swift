2e6b0d532f8748ab86727a008f2f4d55a35cc819
Juan David Hurtado <juandahurtadom@gmail.com>
2023-06-21 19:20:49 -0500

    Add more circles to the fourier series

--- P5Demo/P5Demo/Examples/Fourier Series.swift
+++ P5Demo/P5Demo/Examples/Fourier Series.swift
@@ -11,25 +11,47 @@ import P5Swift
 
 class FourierSeries: P5Sketch {
     var time = 0.0
+    var wave: [CGFloat] = []
     
     override func setup() {
+        frameRate(60)
         title = "Fourier Series"
     }
     
     override func draw() {
         background(UIColor.black.cgColor)
-        translate(150, 350)
-        let radius = 100.0
-        noFill()
-        stroke(UIColor.white.cgColor)
-        circle(0, 0, radius)
+        translate(80, 350)
         
-        let y = sin(time) * radius
-        let x = cos(time) * radius
-        fill(UIColor.white.cgColor)
-        line(0, 0, x, y)
-        circle(x, y, 8)
+        var x = 0.0
+        var y = 0.0
+        for i in 0..<4 {
+            let prevX = x
+            let prevY = y
+            let n = Double(i) * 2.0 + 1.0
+            let radius = 4 / n * .pi  * 5.0
+            noFill()
+            stroke(UIColor.white.withAlphaComponent(0.3).cgColor)
+            circle(prevX, prevY, radius)
+            
+            y += radius * sin(n * time)
+            x += radius * cos(n * time)
+            stroke(UIColor.white.cgColor)
+            line(prevX, prevY, x, y)
+        }
         
-        time -= 0.01
+        wave.insert(y, at: 0)
+        // TODO: use a path for drawing the shape
+        translate(120, 0)
+        line(-120 + x, y, 0, wave[0])
+        for index in wave.indices {
+            fill(UIColor.white.cgColor)
+            circle(CGFloat(index), wave[index], 2)
+        }
+        
+        if wave.count > 150 {
+            let _ = wave.popLast()
+        }
+        
+        time -= 0.03
     }
 }
